bf090ba3260e4b9fb1379c7f494d794e95cca8b2
Hendrik Molder <hendrik@molder.eu>
2018-11-19 21:28:14 +0000

    Add MoveTurn

--- lua-agent/move.lua
+++ lua-agent/move.lua
@@ -21,3 +21,17 @@ end
 function Move:getHole ()
     return self.hole
 end
+
+-- class method for MoveTurn
+MoveTurn = { endMove = false, again = false, move = nil }
+
+function MoveTurn:new (o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index    = self
+    self.endMove    = false
+    self.again      = false
+    self.endMove    = nil
+
+    return o
+end
