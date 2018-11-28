e756f496a0212f2cead18a4185c45f1f329351ef
Aayush Chadha <achadharocks@gmail.com>
2018-11-28 18:33:48 +0000

    Add side class

--- lua-agent/side.lua
+++ lua-agent/side.lua
@@ -6,3 +6,21 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+-- Lua doesn't support enums out of the box, a decent halfway house is using the mapping below
+-- Taken from - https://rosettacode.org/wiki/Enumerations#Lua
+Side = {NORTH = 1, SOUTH = 2}
+
+Side.__index = Side
+
+function Side:getOpposite(side)
+    if side == self.NORTH then
+        return self.SOUTH
+    end
+
+    return self.NORTH
+end
+
+return Side
+
+
+
