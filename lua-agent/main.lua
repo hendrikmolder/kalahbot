b134f690d284671a626d2bc78427c9bf8eeedf69
Aayush Chadha <achadharocks@gmail.com>
2018-11-30 14:56:49 +0000

    Add main game loop

--- lua-agent/main.lua
+++ lua-agent/main.lua
@@ -8,15 +8,70 @@
 
 Protocol = require 'protocol'
 Board = require 'board'
+Move = require 'move'
+Kalah = require 'kalah'
+Side = require 'Side'
 
-testBoard = Board:new(7,7)
+Main = {}
 
-function sendMsg (messageString)
-    io.write(messageString)
+-- Define game "object"
+function Main:new (o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
-function rcvMsg ()
-    local messageString = io.read()
-    return messageString
+function Main:sendMsg(msg)
+    io.write(msg, '\n')
 end
 
+-- DONE this should have some sort of check
+function Main:readMsg()
+    local msg = io.read()
+    print (msg)
+    if (msg == nil) then
+        error("Unexpected end of input")
+    end
+    return msg
+end
+
+-- DONE Change function calls based on game messages perhaps
+function Main:gameLoop()
+    local state = Kalah
+    while true do
+        local msg = Main:readMsg()
+        local messageType = Protocol.getMessageType(msg)
+        if messageType == "START" then
+            local isFirst = Protocol.evaluateStateMsg(msg)
+            if (isFirst) then
+                local move = Move:new(Side.NORTH, 1) -- TODO FIND MOVE
+                sendMsg(Protocol.createMoveMsg(move.hole))
+            else
+                state:setOurSide(Side.NORTH)
+            end
+        elseif messageType == "CHANGE" then
+            local turn = Protocol.evaluateStateMsg(msg)
+            state:performMove(Move:new(state.sideToMove, turn.move))
+            if not turn.endMove then
+                if turn.again then
+                    local move = Move:new(Side.NORTH, 1)
+                    if move.hole == 0 then
+                        sendMsg(Protocol.createSwapMsg())
+                    else
+                        sendMsg(Protocol.createMoveMessage(move.hole))
+                    end
+                end
+            end
+
+        elseif messageType == "END\n" then
+            break
+
+        end
+    end
+end
+
+
+game = Main
+-- game:GameLoop()
+game:gameLoop()
