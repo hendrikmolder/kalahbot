42b1f86dfe8bb80f40f30dfec2de256502657145
Hendrik Molder <hendrik@molder.eu>
2018-12-01 18:17:52 +0000

    Refactor non-objects to be lowercase

--- lua-agent/main.lua
+++ lua-agent/main.lua
@@ -1,9 +1,9 @@
-Protocol = require 'protocol'
+protocol = require 'protocol'
 Board = require 'board'
 Move = require 'move'
 Kalah = require 'kalah'
 Side = require 'side'
-Log = require 'utils.log'
+log = require 'utils.log'
 
 Main = {}
 
@@ -30,33 +30,33 @@ end
 
 -- DONE Change function calls based on game messages perhaps
 function Main:gameLoop()
-    Log.info('gameLoop() started.')
+    log.info('gameLoop() started.')
     local state = Kalah
 
     while true do
         local msg = Main:readMsg()
-        Log.info("Message Received:", msg)
-        local messageType = Protocol.getMessageType(msg)
+        log.info("Message Received:", msg)
+        local messageType = protocol.getMessageType(msg)
 
         if messageType == "start" then
-            local isFirst = Protocol.evaluateStartMsg(msg)
-            Log.info("Message is first:", isFirst)
+            local isFirst = protocol.evaluateStartMsg(msg)
+            log.info("Message is first:", isFirst)
             if (isFirst) then
                 local move = Move:new(Side.NORTH, 1) -- TODO FIND MOVE
-                sendMsg(Protocol:createMoveMsg(move.hole))
+                sendMsg(protocol.createMoveMsg(move.hole))
             else
                 state:setOurSide(Side.NORTH)
             end
         elseif messageType == "change" then
-            local turn = Protocol.evaluateStateMsg(msg)
+            local turn = protocol.evaluateStateMsg(msg)
             state:performMove(Move:new(state.sideToMove, turn.move))
             if not turn.endMove then
                 if turn.again then
                     local move = Move:new(Side.NORTH, 1)
                     if move.hole == 0 then
-                        sendMsg(Protocol:createSwapMsg())
+                        sendMsg(protocol.createSwapMsg())
                     else
-                        sendMsg(Protocol:createMoveMessage(move.hole))
+                        sendMsg(protocol.createMoveMessage(move.hole))
                     end
                 end
             end
@@ -66,7 +66,7 @@ function Main:gameLoop()
 
         end
     end
-    Log.info('gameLoop() stopped.')
+    log.info('gameLoop() stopped.')
 end
 
 
