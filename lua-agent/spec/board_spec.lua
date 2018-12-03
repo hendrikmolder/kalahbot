c3dca6707b29fdee2e2cba81be1dcca296704500
Hendrik Mölder <hendrik@molder.eu>
2018-12-03 15:30:03 +0000

    Fix Side enum usage in board spec

--- lua-agent/spec/board_spec.lua
+++ lua-agent/spec/board_spec.lua
@@ -9,8 +9,8 @@
 board = require 'board'
 Side = require 'side'
 
-northSideString = "NORTH"
-southSideString = "SOUTH"
+Side.NORTH = "NORTH"
+Side.SOUTH = "SOUTH"
 
 
 testBoard = board:new(nil, 7,7)
@@ -71,20 +71,20 @@ end)
 
 describe('getSeeds', function()
     it('returns seeds in a hole', function ()
-        assert.equals(testBoard:getSeeds(northSideString, 4), 7)
+        assert.equals(testBoard:getSeeds(Side.NORTH, 4), 7)
     end)
 end)
 
 describe('setSeeds', function()
     it('sets the seeds on a specified side and hole', function()
-        testBoard:setSeeds(northSideString, 4, 3)
-        assert.equals(testBoard:getSeeds(northSideString, 4), 3)
+        testBoard:setSeeds(Side.NORTH, 4, 3)
+        assert.equals(testBoard:getSeeds(Side.NORTH, 4), 3)
     end)
 end)
 
 describe('addSeeds', function ()
     it('adds the number of seeds in a given well', function()
-        testBoard:addSeeds(southSideString, 5, 4)
+        testBoard:addSeeds(Side.SOUTH, 5, 4)
         assert.equals(testBoard:getSeeds(Side.SOUTH, 5), 11)
     end)
 end)
@@ -102,61 +102,61 @@ end)
 
 describe('setSeedsOp', function()
     it('sets the seeds in the opposite (south) well', function()
-        testBoard:setSeedsOp(northSideString, 3, 1)
-        assert.equals(testBoard:getSeedsOp(northSideString, 3), 1)
+        testBoard:setSeedsOp(Side.NORTH, 3, 1)
+        assert.equals(testBoard:getSeedsOp(Side.NORTH, 3), 1)
     end)
 
     it('sets the seeds in the opposite (north) well', function()
-        testBoard:setSeedsOp(southSideString, 2, 1)
-        assert.equals(testBoard:getSeedsOp(southSideString, 2), 1)
+        testBoard:setSeedsOp(Side.SOUTH, 2, 1)
+        assert.equals(testBoard:getSeedsOp(Side.SOUTH, 2), 1)
     end)
 end)
 
 describe('addSeedsOp', function()
     it('adds seeds to the opposite (north) well', function ()
-        testBoard:addSeedsOp(northSideString, 3, 2)
-        assert.equals(testBoard:getSeedsOp(northSideString, 3), 3)
+        testBoard:addSeedsOp(Side.NORTH, 3, 2)
+        assert.equals(testBoard:getSeedsOp(Side.NORTH, 3), 3)
     end)
 
     it('adds seeds to the opposite (south) well', function ()
-        testBoard:addSeedsOp(southSideString, 2, 2)
-        assert.equals(testBoard:getSeedsOp(southSideString, 2), 3)
+        testBoard:addSeedsOp(Side.SOUTH, 2, 2)
+        assert.equals(testBoard:getSeedsOp(Side.SOUTH, 2), 3)
     end)
 
 end)
 
 describe('getSeedsInStore', function()
     it('returns the seeds in the north scoring well', function ()
-        assert.equals(testBoard:getSeedsInStore(northSideString), 0)
+        assert.equals(testBoard:getSeedsInStore(Side.NORTH), 0)
     end)
 
     it('returns the seeds in the south scoring well', function ()
-        assert.equals(testBoard:getSeedsInStore(northSideString), 0)
+        assert.equals(testBoard:getSeedsInStore(Side.NORTH), 0)
     end)
 
 end)
 
 describe('setSeedsInStore', function()
     it('sets the seeds in the north scoring well', function ()
-        testBoard:setSeedsInStore(northSideString, 11)
-        assert.equals(testBoard:getSeedsInStore(northSideString), 11)
+        testBoard:setSeedsInStore(Side.NORTH, 11)
+        assert.equals(testBoard:getSeedsInStore(Side.NORTH), 11)
     end)
 
     it('sets the seeds in the south scoring well', function ()
-        testBoard:setSeedsInStore(southSideString, 11)
-        assert.equals(testBoard:getSeedsInStore(northSideString), 11)
+        testBoard:setSeedsInStore(Side.SOUTH, 11)
+        assert.equals(testBoard:getSeedsInStore(Side.NORTH), 11)
     end)
 end)
 
 describe('addSeedsToStore', function()
     it('adds seeds to the north scoring well', function ()
-        testBoard:addSeedsToStore(northSideString, 2)
-        assert.equals(testBoard:getSeedsInStore(northSideString), 13)
+        testBoard:addSeedsToStore(Side.NORTH, 2)
+        assert.equals(testBoard:getSeedsInStore(Side.NORTH), 13)
     end)
 
     it('adds seeds to the south scoring well', function ()
-        testBoard:addSeedsToStore(southSideString, 2)
-        assert.equals(testBoard:getSeedsInStore(southSideString), 13)
+        testBoard:addSeedsToStore(Side.SOUTH, 2)
+        assert.equals(testBoard:getSeedsInStore(Side.SOUTH), 13)
     end)
 end)
 
