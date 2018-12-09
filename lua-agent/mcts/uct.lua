7c1a79f3b4e89d45af48d379d47ec46337ddfeb0
Aayush Chadha <achadharocks@gmail.com>
2018-12-09 12:30:13 +0000

    Change function signature

--- lua-agent/mcts/uct.lua
+++ lua-agent/mcts/uct.lua
@@ -2,7 +2,7 @@ UCT = {}
 
 local MAX_VALUE = math.huge
 
-function UCT.uctValue(totalVisit, nodeWinScore, nodevisit)
+function UCT.uctValue(totalVisit, nodeWinScore, nodeVisit)
     if (nodeVisit == 0) then return math.huge end
 
     return (nodeWinScore / nodeVisit) + 1.41 * math.sqrt(math.log(totalVisit) / nodeVisit)
